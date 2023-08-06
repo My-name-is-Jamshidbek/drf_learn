baaa0c3f7d44b7e5655a04ee28b0f92e42a5652d
xjama <jamshidbekollanazarov@gmail.com>
2023-08-06 20:53:54 +0500

    blog api crud created

--- blog_project/config/settings.py
+++ blog_project/config/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # additional posts
+    'rest_framework',
+
+    # personal apps
+    'posts',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        "rest_framework.permissions.AllowAny"
+    ]
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
