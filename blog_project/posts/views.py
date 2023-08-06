3007d9c001e285d954c89e11b25447c226dd798d
xjama <jamshidbekollanazarov@gmail.com>
2023-08-06 22:14:40 +0500

    permissons yaratildi va amalga tatbiq etildi

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -2,14 +2,18 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 
 from .serializers import PostSerializer
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostList(ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated, )  # agar ro'yxatdan otgan bo'lsa
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAuthenticated, ) # agar ro'yxatdan otgan bo'lsa
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
